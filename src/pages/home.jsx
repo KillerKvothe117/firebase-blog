56f8727a088e2ff8e81253d89c26f7ab0966acf6
Kosi <iyiegbuk@gmail.com>
2022-06-24 15:05:42 +0100

    added display on home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import { deleteDoc, getDocs } from "firebase/firestore";
+import { db } from "../firebase-config";
+import { collection } from "firebase/firestore";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [postLists, setPostLists] = useState([]);
+  const postCollectionRef = collection(db, "posts");
 
-export default Home
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postCollectionRef);
+      setPostLists(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getPosts();
+  });
+
+  const deletePost = async () => {
+    await deleteDoc()
+  }
+
+  return (
+    <div className="homePage">
+      {postLists.map((post) => {
+        return (
+          <div className="post">
+            <div className="postHeader">
+              <div className="title">
+                <h1>{post.title}</h1>
+              </div>
+              <div className="deletePost">
+                <button>&#128465;</button>
+              </div>
+            </div>
+            <div className="postTextContainer">{post.postText}</div>
+            <h3>@{post.author.name}</h3>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Home;
